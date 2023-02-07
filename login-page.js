43f435b4bc49b9794e62fba84388de294ad8897d
GikitSRC <123793568+GikitSRC@users.noreply.github.com>
2023-02-07 18:44:15 -0500

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -14,6 +14,13 @@ loginButton.addEventListener("click", (e) => {
     } else {
         loginErrorMsg.style.opacity = 1;
     }
+    if (username === "ADMIN" && password === "test") {
+        history.pushState("/newpath", {}, "/newpath");
+        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
+        location.replace('https://classroom.google.com')
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
     if (username === "HelloDudWow" && password === "ERICISALEBA") {
         history.pushState("/newpath", {}, "/newpath");
         open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
