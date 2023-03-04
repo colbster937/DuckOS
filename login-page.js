88e6906c2982e00001f6bb8b666d7448769b45b5
GikitSRC <123793568+GikitSRC@users.noreply.github.com>
2023-03-03 19:06:52 -0800

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -13,100 +13,72 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "ADMIN" && password === "test") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/testing" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "HelloDudWow" && password === "ERICISALEBA") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Yoshi" && password === "97193") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "NeilM" && password === "123456") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "evan" && password === "123456") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "ghgi" && password === "443") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "g4o2" && password === "god") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "my name" && password === "byeadmin") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Neel" && password === "4235") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "afterwords" && password === "123456789") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Elena" && password === "1234") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Momo" && password === "Marshmallow") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "rushilc" && password === "Rushil2009!!") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Krish" && password === "Bhejafry13@1234") {
-        history.pushState("/newpath", {}, "/newpath");
-        open().document.write('<iframe src="//skyhax.lol/233e003ek3wdfsdf323rf-234wdf" frameborder="0" style="width:100%; height:100%; border: 0px solid black;"></iframe>')
-        location.replace('https://classroom.google.com')
+        open("/redirect.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
