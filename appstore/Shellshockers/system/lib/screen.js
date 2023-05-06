5b1759676ea523cceb8607d98779764b9ab619b4
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-05-05 22:02:20 -0700

    Delete appstore directory

--- appstore/Shellshockers/system/lib/screen.js
+++
@@ -1,9 +0,0 @@
-const windows = []
-const maximized = []
-const minimized = []
-
-function showTaskBar() {
-    target = document.getElementById('taskbar');
-    if (target.style.top == '0px') { target.style.top = '-35px'; }
-    else { target.style.top = '0px'; }
-}
