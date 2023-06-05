f95843ba7d011230c6bdfd057d90f64e6e09f782
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-06-05 16:56:39 -0700

    Add files via upload

--- system/lib/settings.js
+++ system/lib/settings.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const settingsbtn = document.getElementById('settingsbtn');
     const settingsPage = document.getElementById('settingspage');
+    const toggleSwitch = document.getElementById('toggleSwitch');
 
     function toggleSettings(e) {
         e.stopPropagation();
@@ -29,9 +30,55 @@ document.addEventListener('DOMContentLoaded', function() {
         e.stopPropagation();
     }
 
+   function toggleMode() {
+    document.body.classList.toggle('light-mode');
+    toggleSwitch.textContent = document.body.classList.contains('light-mode') ? 'Light Mode' : 'Dark Mode';
+
+    if (document.body.classList.contains('light-mode')) {
+        document.getElementById('settingspage').style.backgroundColor = 'white';
+        document.getElementById('taskbar').style.backgroundColor = 'white';
+        document.getElementById('statscenter').style.backgroundColor = 'white';
+        document.getElementById('time').style.backgroundColor = 'white';
+        document.getElementById('time').style.color = 'black';
+        document.getElementById('settingsbtn').style.backgroundColor = 'white';
+        document.getElementById('settingsbtn').style.color = 'black';
+        document.getElementById('fullscreenbtn').style.backgroundColor = 'white';
+        document.getElementById('fullscreenbtn').style.color = 'black';
+        document.getElementById('settingspage').style.color = 'black';
+        document.getElementById('taskbar').style.color = 'black';
+        document.getElementById('statscenter').style.color = 'black';
+        document.getElementById('statscenterbtn').style.backgroundColor = 'white';
+        document.getElementById('statscenterbtn').style.color = 'black';
+    } else {
+        document.getElementById('settingspage').style.backgroundColor = '';
+        document.getElementById('taskbar').style.backgroundColor = '';
+        document.getElementById('statscenter').style.backgroundColor = '';
+        document.getElementById('time').style.backgroundColor = '';
+        document.getElementById('time').style.color = '';
+        document.getElementById('settingsbtn').style.backgroundColor = '';
+        document.getElementById('settingsbtn').style.color = '';
+        document.getElementById('fullscreenbtn').style.backgroundColor = '';
+        document.getElementById('fullscreenbtn').style.color = '';
+        document.getElementById('settingspage').style.color = '';
+        document.getElementById('taskbar').style.color = '';
+        document.getElementById('statscenter').style.color = '';
+        document.getElementById('statscenterbtn').style.backgroundColor = '';
+        document.getElementById('statscenterbtn').style.color = '';
+    }
+}
+
+
+
     settingsbtn.addEventListener('click', toggleSettings);
     settingsPage.addEventListener('click', preventPropagation);
     document.addEventListener('click', hideSettings);
+    toggleSwitch.addEventListener('click', toggleMode);
+
+    const savedBackground = localStorage.getItem('background');
+    if (savedBackground) {
+        document.body.style.backgroundImage = savedBackground;
+        document.body.style.backgroundSize = 'cover';
+    }
 });
 
 function uploadBackground() {
@@ -41,9 +88,12 @@ function uploadBackground() {
     reader.onload = function(e) {
         document.body.style.backgroundImage = "url('" + e.target.result + "')";
         document.body.style.backgroundSize = "cover";
+
+        localStorage.setItem('background', "url('" + e.target.result + "')");
     }
     reader.readAsDataURL(file);
 }
+
 let selectedImage = null;
 
 function selectedimg(img) {
@@ -56,16 +106,24 @@ function selectedimg(img) {
 
 function changeBackgroundefualt() {
     document.body.style.backgroundImage = "url('/system/media/background.jpg')";
+
+    localStorage.setItem('background', "url('/system/media/background.jpg')");
 }
 
 function changeBackgroundmeow() {
     document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095511342527557762/838-transformed.jpeg')";
+
+    localStorage.setItem('background', "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095511342527557762/838-transformed.jpeg')");
 }
 
 function changeBackgroundmeow1() {
     document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095511342972162180/CbV7Rn7-earth-desktop-wallpaper.jpg')";
+
+    localStorage.setItem('background', "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095511342972162180/CbV7Rn7-earth-desktop-wallpaper.jpg')");
 }
 
 function changeBackgroundbeefy() {
-    document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095737363503796264/Screenshot_2023-04-11_6.png')"
+    document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095737363503796264/Screenshot_2023-04-11_6.png')";
+
+    localStorage.setItem('background', "url('https://cdn.discordapp.com/attachments/1095511008698704033/1095737363503796264/Screenshot_2023-04-11_6.png')");
 }
