624ef708b17f7eb488fc2cdcfaf4819ff2df9098
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-09-05 21:55:35 -0700

    Add files via upload

--- system/apps/games/script.js
+++ system/apps/games/script.js
@@ -1,84 +1,84 @@
 const config = {
-    images: [
-      {
-        title: "Minecraft",
-        url: "gamefiles/minecraft/icon.png",
-        link: "gamefiles/minecraft/index.html"
-      },
-      {
-        title: "Legend of Zelda",
-        url: "icons/zelda.png",
-        link: "https://duckos-games.pages.dev/zelda/"
-      },
-      {
-        title: "2048",
-        url: "icons/2048.png",
-        link: "https://duckos-games.pages.dev/2048/"
-      },
-      {
-        title: "paper.io",
-        url: "icons/paperio.jpg",
-        link: "https://duckos-games.pages.dev/paperio2/"
-      },
-      {
-        title: "Tetris",
-        url: "icons/tetris.png",
-        link: "https://duckos-games.pages.dev/tetris/"
-      },
-      {
-        title: "Pokemon Emerald",
-        url: "icons/emerald.jpg",
-        link: "https://duckos-games.pages.dev/pokemon/emerald/"
-      },
-      {
-        title: "Pokemon Fire Red",
-        url: "icons/fire.jpg",
-        link: "https://duckos-games.pages.dev/pokemon/fire-red/"
-      },
-      {
-        title: "Pokemon Chaos Black",
-        url: "icons/black.jpg",
-        link: "https://duckos-games.pages.dev/pokemon/black/"
-      },
-    ]
-  };
+  images: [
+    {
+      title: "Minecraft",
+      url: "gamefiles/minecraft/icon.png",
+      link: "gamefiles/minecraft/index.html"
+    },
+    {
+      title: "Legend of Zelda",
+      url: "icons/zelda.png",
+      link: "https://duckos-games.pages.dev/zelda/"
+    },
+    {
+      title: "2048",
+      url: "icons/2048.png",
+      link: "https://duckos-games.pages.dev/2048/"
+    },
+    {
+      title: "paper.io",
+      url: "icons/paperio.jpg",
+      link: "https://duckos-games.pages.dev/paperio2/"
+    },
+    {
+      title: "Tetris",
+      url: "icons/tetris.png",
+      link: "https://duckos-games.pages.dev/tetris/"
+    },
+    {
+      title: "Pokemon Emerald",
+      url: "icons/emerald.jpg",
+      link: "https://duckos-games.pages.dev/pokemon/emerald/"
+    },
+    {
+      title: "Pokemon Fire Red",
+      url: "icons/fire.jpg",
+      link: "https://duckos-games.pages.dev/pokemon/fire-red/"
+    },
+    {
+      title: "Pokemon Chaos Black",
+      url: "icons/black.jpg",
+      link: "https://duckos-games.pages.dev/pokemon/black/"
+    },
+  ]
+};
 
-  const gallery = document.getElementById('gallery');
-  const searchInput = document.getElementById('searchInput');
+const gallery = document.getElementById('gallery');
+const searchInput = document.getElementById('searchInput');
 
-  function renderGallery(images) {
-    gallery.innerHTML = '';
-    images.forEach(image => {
-      const imageContainer = document.createElement('a');
-      imageContainer.className = 'image-container';
-      imageContainer.href = image.link;
+function renderGallery(images) {
+  gallery.innerHTML = '';
+  images.forEach(image => {
+    const imageContainer = document.createElement('a');
+    imageContainer.className = 'image-container';
+    imageContainer.href = image.link;
 
-      const img = document.createElement('img');
-      img.className = 'image';
-      img.src = image.url;
-      img.alt = image.title;
+    const img = document.createElement('img');
+    img.className = 'image';
+    img.src = image.url;
+    img.alt = image.title;
 
-      const content = document.createElement('div');
-      content.className = 'content';
+    const content = document.createElement('div');
+    content.className = 'content';
 
-      const title = document.createElement('div');
-      title.className = 'image-title';
-      title.textContent = image.title;
+    const title = document.createElement('div');
+    title.className = 'image-title';
+    title.textContent = image.title;
 
-      content.appendChild(title);
-      imageContainer.appendChild(img);
-      imageContainer.appendChild(content);
-      gallery.appendChild(imageContainer);
-    });
-  }
-
-  function filterImages(searchTerm) {
-    const filteredImages = config.images.filter(image =>
-      image.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    renderGallery(filteredImages);
-  }
-  renderGallery(config.images);
-  searchInput.addEventListener('input', () => {
-    filterImages(searchInput.value);
+    content.appendChild(title);
+    imageContainer.appendChild(img);
+    imageContainer.appendChild(content);
+    gallery.appendChild(imageContainer);
   });
+}
+
+function filterImages(searchTerm) {
+  const filteredImages = config.images.filter(image =>
+    image.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  renderGallery(filteredImages);
+}
+renderGallery(config.images);
+searchInput.addEventListener('input', () => {
+  filterImages(searchInput.value);
+});
